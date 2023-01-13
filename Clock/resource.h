b895a1092a132a767cf2892e7763a96502003a5c
sosuisen <hidekazu.kubota@gmail.com>
2023-01-13 15:40:25 +0900

    fix: remove resource

--- Clock/resource.h
+++ Clock/resource.h
@@ -2,18 +2,3 @@
 // Microsoft Visual C++ で生成されたインクルード ファイル。
 // Clock.rc で使用
 //
-#define IDD_DIALOG1                     102
-#define IDC_LIST1                       1007
-#define IDC_COMBO1                      1009
-#define IDC_COMBO2                      1010
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1011
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
